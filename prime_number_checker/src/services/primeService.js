8fa482a14205e048bffba53f8e82cd26e7652901
Filius Arthur Lingga <filiusarthur@gmail.com>
2025-11-28 10:00:17 +0700

    Minor fixes

--- prime_number_checker/src/services/primeService.js
+++ prime_number_checker/src/services/primeService.js
@@ -1,11 +1,11 @@
 const API_BASE_URL = import.meta.env.PROD
     ? '/api'
-    : 'http://localhost:3001';
+    : 'http://localhost:3001/api';
 
 
 export const checkPrime = async (number) => {
     try{
-        const response = await fetch(`${API_BASE_URL}/check_prime/${number}`);
+        const response = await fetch(`${API_BASE_URL}/check-prime/${number}`);
         if(!response.ok){
             const errorData = await response.json();
             throw new Error(errorData.message || 'Server Error');
